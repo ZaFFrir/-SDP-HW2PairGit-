6ca188846a9cbece4652401e9a13a0c6239cd4f4
Vasita Sreekaew <sr.vasita_st@tni.ac.th>
2016-12-16 04:38:15 +0000

    fifth commit

--- PyTest/RandomNum9.py
+++ PyTest/RandomNum9.py
@@ -1,1 +1,4 @@
-hfghjf
+from random import randrange
+lotto3=randrange(0,99999,9)
+print(lotto3*10+9)
+print('\n')
--- lotto.py
+++ lotto.py
@@ -11,13 +11,11 @@ else :
         print('\n')
         
 for i in range(4):
-<<<<<<< HEAD
         lotto2=randint(100,999)
         print(lotto2)
 
 lotto3=randint(10,99)
 print(lotto3)
-=======
     lotto2=randint(100,999)
     print(lotto2)
 print('\n')
@@ -25,4 +23,4 @@ print('\n')
 lotto3=randint(11,99)
 print(lotto3)
 print('\n')
->>>>>>> e89244438501c9dbcb1bbf5c0d7a24c04da8befe
+
