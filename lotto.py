e57b7736eba43394790c8d2858e1ae6fdb4cd06d
Vasita Sreekaew <sr.vasita_st@tni.ac.th>
2016-12-16 03:56:50 +0000

    secound commit

---
+++ lotto.py
@@ -0,0 +1,10 @@
+from random import randint
+first=186069
+lotto=randint(000000,999999)
+if lotto==first:
+        print('Congratulation!!!')
+        print(lotto)
+else :
+        print('You miss prize!')
+        print(lotto)
+
