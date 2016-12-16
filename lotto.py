e89244438501c9dbcb1bbf5c0d7a24c04da8befe
Vasita Sreekaew <sr.vasita_st@tni.ac.th>
2016-12-16 04:17:42 +0000

    fifth commit

--- lotto.py
+++ lotto.py
@@ -4,10 +4,17 @@ lotto=randint(000000,999999)
 if lotto==first:
         print('Congratulation!!!')
         print(lotto)
+        print('\n')
 else :
         print('You miss prize!')
         print(lotto)
+        print('\n')
         
 for i in range(4):
-        lotto2=randint(100,999)
-        print(lotto2)
+    lotto2=randint(100,999)
+    print(lotto2)
+print('\n')
+        
+lotto3=randint(11,99)
+print(lotto3)
+print('\n')
